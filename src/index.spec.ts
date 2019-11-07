709c15c18ce2aa71bb922c705007688db58e9c90
Andy Edwards <jedwards@fastmail.com>
2019-11-06 22:52:06 -0600

    build: fix mocha config, prettier glob

---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+/* eslint-env mocha */
+
+describe('test setup', () => {
+  it('works', () => {})
+})
--- test/configure.js
+++ test/configure.js
@@ -1,4 +1,7 @@
-/* eslint-env node */
+/* eslint-disable */
+
+const { before } = require('mocha')
+require('@babel/register')({ extensions: ['.js', '.ts'] })
 
 if (process.argv.indexOf('--watch') >= 0) {
   before(() => process.stdout.write('\u001b[2J\u001b[1;1H\u001b[3J'))
