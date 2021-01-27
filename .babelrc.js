1febd542942e63f8bd85af523962dc6461b3fefd
Andy Edwards <jedwards@fastmail.com>
2021-01-27 15:26:17 -0600

    chore: use node 14, upgrade deps, fix various issues

--- .babelrc.js
+++ .babelrc.js
@@ -1,21 +1,13 @@
-module.exports = function(api) {
-  const plugins = [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ]
+module.exports = function (api) {
+  const plugins = ['@babel/plugin-proposal-class-properties']
   const presets = [
     [
       '@babel/preset-env',
       api.env('es5')
         ? { forceAllTransforms: true }
-        : { targets: { node: 'current' } },
+        : { targets: { node: '12' } },
     ],
-    '@babel/preset-typescript',
+    ['@babel/preset-typescript', { allowDeclareFields: true }],
   ]
 
   if (api.env(['test', 'coverage', 'es5'])) {
