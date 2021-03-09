4a2059e5c35531d43b826b10a123b8a1d6fb86b0
Andy Edwards <jedwards@fastmail.com>
2021-03-08 23:32:43 -0600

    refactor(Identifier): simplify code

--- src/compileMatcher/Identifier.ts
+++ src/compileMatcher/Identifier.ts
@@ -1,37 +1,15 @@
-import { ASTPath, Identifier } from 'jscodeshift'
-import { CompiledMatcher, CompileOptions, convertPredicateMatcher } from './'
+import { Identifier } from 'jscodeshift'
+import { CompiledMatcher, CompileOptions } from './'
 import compileCaptureMatcher, { unescapeIdentifier } from './Capture'
 import compileGenericNodeMatcher from './GenericNodeMatcher'
 
 export default function compileIdentifierMatcher(
   query: Identifier,
   compileOptions: CompileOptions
-): CompiledMatcher {
-  const { debug } = compileOptions
+): CompiledMatcher | void {
   if (query.typeAnnotation != null)
     return compileGenericNodeMatcher(query, compileOptions)
   const captureMatcher = compileCaptureMatcher(query.name, compileOptions)
   if (captureMatcher) return captureMatcher
-  const name = unescapeIdentifier(query.name)
-  return convertPredicateMatcher(
-    query,
-    {
-      predicate: true,
-      match: (path: ASTPath): boolean => {
-        debug('Identifier', name)
-        if (path.node?.type !== 'Identifier') {
-          debug(`  node is a %s`, path.node?.type)
-          return false
-        } else if (path.node.name === name) {
-          debug('  %s === %s', path.node.name, name)
-          return true
-        } else {
-          debug(`  %s !== %s`, path.node.name, name)
-          return false
-        }
-      },
-      nodeType: 'Identifier',
-    },
-    compileOptions
-  )
+  query.name = unescapeIdentifier(query.name)
 }
