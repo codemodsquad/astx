fd5f8b7c3f36d79084fd3f34b6b96026c1f493d9
Andy Edwards <jedwards@fastmail.com>
2021-04-25 12:51:25 -0500

    fix(parseFindOrReplace): default to function and class declarations instead of expressions

--- src/util/parseFindOrReplace.ts
+++ src/util/parseFindOrReplace.ts
@@ -1,4 +1,9 @@
-import { Expression, JSCodeshift, Statement } from 'jscodeshift'
+import {
+  Expression,
+  ExpressionStatement,
+  JSCodeshift,
+  Statement,
+} from 'jscodeshift'
 import template from './template'
 
 export default function parseFindOrReplace(
@@ -8,11 +13,15 @@ export default function parseFindOrReplace(
 ): Expression | Statement | Statement[] {
   const { expression, statements } = template(j)
   try {
-    return expression(strings, ...quasis)
+    const result = statements(strings, ...quasis)
+    if (result.length === 1) {
+      if (result[0].type === 'ExpressionStatement')
+        return (result[0] as ExpressionStatement).expression
+      return result[0]
+    }
+    if (result.length) return result
   } catch (error) {
-    // ignore
+    // fallthrough
   }
-
-  const result = statements(strings, ...quasis)
-  return result.length === 1 ? result[0] : result
+  return expression(strings, ...quasis)
 }
