420a5e0e4bcad7275cba003f56a2a317d32bfba4
Andy Edwards <jedwards@fastmail.com>
2021-08-12 17:05:52 -0500

    fix: string capture escaping

--- test/findReplace/testcases/templateLiteralEscape.ts
+++ test/findReplace/testcases/templateLiteralEscape.ts
@@ -5,3 +5,9 @@ const x = \`$foo\`
 export const find = `\`$_foo\``
 
 export const expectedFind = [{ node: '`$foo`' }]
+
+export const replace = `\`$_bar\``
+
+export const expectedReplace = `
+const x = \`$bar\`
+`
