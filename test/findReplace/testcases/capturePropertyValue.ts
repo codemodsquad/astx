187dbe95706df820678d8e18dbfe009e58af3357
Andy Edwards <jedwards@fastmail.com>
2022-08-17 15:16:44 -0500

    test: add test to make sure previous commit didn't cause regression

---
+++ test/findReplace/testcases/capturePropertyValue.ts
@@ -0,0 +1,22 @@
+export const input = `
+const x = { a: c, b }
+`
+
+export const find = `
+const x = { /**/ a: $a }
+`
+
+export const expectedFind = [
+  {
+    node: 'a: c',
+    captures: { $a: 'c' },
+  },
+]
+
+export const replace = `
+const x = { /**/ c: $a }
+`
+
+export const expectedReplace = `
+const x = { c: c, b }
+`
