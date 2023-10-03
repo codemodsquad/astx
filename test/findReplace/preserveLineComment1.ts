877fd35b11e2b93947a7a5d1ac057fda52bc434e
Andy Edwards <jedwards@fastmail.com>
2023-10-03 12:31:54 -0500

    test: add failing comment tests

---
+++ test/findReplace/preserveLineComment1.ts
@@ -0,0 +1,21 @@
+import { findReplaceTestcase } from '../findReplaceTestcase'
+
+findReplaceTestcase({
+  file: __filename,
+  input: `
+    const foo = 1
+    // test
+    const bar = 2
+  `,
+  find: `
+    const $x = $y
+  `,
+  replace: `
+    const $x = a($y)
+  `,
+  expectedReplace: `
+    const foo = a(1)
+    // test
+    const bar = a(2)
+  `,
+})
