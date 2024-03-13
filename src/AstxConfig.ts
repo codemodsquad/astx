0ff4a99ef7d44a8a893bf7fb1454aad40be2610a
Andy Edwards <jedwards@fastmail.com>
2024-03-12 23:52:21 -0500

    fix: use zod instead of typed-validators

--- src/AstxConfig.ts
+++ src/AstxConfig.ts
@@ -1,38 +1,23 @@
 import { Backend } from './backend/Backend'
-import * as t from 'typed-validators'
+import z from 'zod'
 import debug from 'debug'
 
 export const debugConfig = debug('astx:config')
 
-export type AstxConfig = {
-  parser?:
-    | 'babel'
-    | 'babel/auto'
-    | 'recast/babel'
-    | 'recast/babel/auto'
-    | Backend
-  parserOptions?: Record<string, any>
-  workers?: number
-  prettier?: boolean
-  preferSimpleReplacement?: boolean
-}
+export const AstxConfigType = z.strictObject({
+  parser: z
+    .union([
+      z.literal('babel'),
+      z.literal('babel/auto'),
+      z.literal('recast/babel'),
+      z.literal('recast/babel/auto'),
+      z.instanceof(Backend),
+    ])
+    .optional(),
+  parserOptions: z.record(z.string(), z.any()).optional(),
+  workers: z.number().optional(),
+  prettier: z.boolean().optional(),
+  preferSimpleReplacement: z.boolean().optional(),
+})
 
-export const AstxConfigType: t.TypeAlias<AstxConfig> = t.alias(
-  'AstxConfig',
-  t.object({
-    optional: {
-      parser: t.oneOf(
-        t.string('babel'),
-        t.string('babel/auto'),
-        t.string('recast/babel'),
-        t.string('recast/babel/auto'),
-        t.instanceOf(() => Backend)
-      ),
-
-      parserOptions: t.record(t.string(), t.any()),
-      workers: t.number(),
-      prettier: t.boolean(),
-      preferSimpleReplacement: t.boolean(),
-    },
-  })
-)
+export type AstxConfig = z.output<typeof AstxConfigType>
