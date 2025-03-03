e793b2c5ad752432b3d02c41f34f3fd38d31eb48
Andy Edwards <jedwards@fastmail.com>
2025-03-02 23:38:33 -0600

    chore: check for Node >=20.6.0 before registering ts-node ESM loader

--- src/node/registerTsNode.ts
+++ src/node/registerTsNode.ts
@@ -5,7 +5,12 @@ export const registerTsNode = once(async () => {
     const { register } = await import('ts-node')
     register({ transpileOnly: true })
   }
-  {
+  const procMatch = /^v?(\d+)\.(\d+)\.(\d+)$/.exec(process.version)
+  if (
+    procMatch &&
+    parseInt(procMatch[1]) >= 20 &&
+    (parseInt(procMatch[1]) > 20 || parseInt(procMatch[2]) > 6)
+  ) {
     const { register } = await import('./registerTsNodeEsm')
     await register()
   }
